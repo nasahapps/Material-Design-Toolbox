ccc11470cac9de75d2c6d50fde36eadf3f95dd5d
Hakeem <nasahapps@gmail.com>
2016-04-12 20:01:55 -0700

    Initial commit

---
+++ app/src/main/java/com/nasahapps/materialdesigntoolbox/example/MainActivity.java
@@ -0,0 +1,12 @@
+package com.nasahapps.materialdesigntoolbox.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
