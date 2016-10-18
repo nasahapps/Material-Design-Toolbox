ac37b34e65d1544dbf995e2984d7c75eecc49c12
Hakeem <nasahapps@gmail.com>
2016-10-17 22:46:04 -0700

    Removed my own library from sample app (unnecessary)

--- app/src/main/java/com/nasahapps/mdt/example/ui/BaseActivity.kt
+++ app/src/main/java/com/nasahapps/mdt/example/ui/BaseActivity.kt
@@ -1,13 +1,26 @@
 package com.nasahapps.mdt.example.ui
 
+import android.os.Bundle
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
+import android.support.annotation.LayoutRes
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by Hakeem on 4/13/16.
  */
-abstract class BaseActivity : com.nasahapps.nasahutils.app.BaseActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    @LayoutRes
+    protected open fun getLayoutId() = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (getLayoutId() != 0) {
+            setContentView(getLayoutId())
+        }
+    }
 
     @ColorInt
     fun getColorInt(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
--- app/src/main/java/com/nasahapps/mdt/example/ui/BaseFragment.kt
+++ app/src/main/java/com/nasahapps/mdt/example/ui/BaseFragment.kt
@@ -1,14 +1,33 @@
 package com.nasahapps.mdt.example.ui
 
 import android.graphics.Color
+import android.os.Bundle
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
+import android.support.annotation.LayoutRes
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.nasahapps.mdt.example.ui.main.MainActivity
 
 /**
  * Created by Hakeem on 4/13/16.
  */
-abstract class BaseFragment : com.nasahapps.nasahutils.app.BaseFragment() {
+abstract class BaseFragment : Fragment() {
+
+    @LayoutRes
+    protected open fun getLayoutId() = 0
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = if (getLayoutId() != 0) {
+            inflater?.inflate(getLayoutId(), container, false)
+        } else {
+            null
+        }
+
+        return v
+    }
 
     @ColorInt
     protected fun getColorInt(@ColorRes colorRes: Int) = (activity as? MainActivity)?.getColorInt(colorRes) ?: Color.BLACK
