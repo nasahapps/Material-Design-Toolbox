0ef25530b92caa00d53661e09b98a817a2d53d9f
Hakeem <nasahapps@gmail.com>
2017-03-19 17:19:58 -0700

    Removed bottomnav module for Google's BottomNavigationView (design library)

--- app/src/main/java/com/nasahapps/mdt/example/ui/TestActivity.kt
+++ app/src/main/java/com/nasahapps/mdt/example/ui/TestActivity.kt
@@ -1,8 +1,9 @@
 package com.nasahapps.mdt.example.ui
 
 import android.os.Bundle
-import com.nasahapps.mdt.MaterialAnimationUtils
+import android.support.v7.widget.LinearLayoutManager
 import com.nasahapps.mdt.example.R
+import com.nasahapps.mdt.example.ui.mock.MockRecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : BaseActivity() {
@@ -14,9 +15,8 @@ class TestActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        button?.setOnClickListener {
-            MaterialAnimationUtils.createChangeBoundsAnimation(fab, bottomView)
-        }
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+        recyclerView?.adapter = MockRecyclerViewAdapter()
     }
 
 }
--- app/src/main/java/com/nasahapps/mdt/example/ui/components/BottomNavigationFragment.kt
+++ app/src/main/java/com/nasahapps/mdt/example/ui/components/BottomNavigationFragment.kt
@@ -1,10 +1,8 @@
 package com.nasahapps.mdt.example.ui.components
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.view.View
-import com.nasahapps.mdt.bottomnavigation.BottomNavigationBar
 import com.nasahapps.mdt.example.R
 import com.nasahapps.mdt.example.ui.main.MainActivity
 import com.nasahapps.mdt.example.ui.mock.MockFragment
@@ -18,6 +16,7 @@ class BottomNavigationFragment : ComponentFragment() {
     companion object {
         val EXTRA_NUM_TABS = "numTabs"
         val EXTRA_COLORED = "colored"
+        private val BUNDLE_SELECTED_ITEM = "selectedItem"
 
         fun newInstance(numOfTabs: Int, colored: Boolean = false): BottomNavigationFragment {
             val args = Bundle()
@@ -29,11 +28,10 @@ class BottomNavigationFragment : ComponentFragment() {
         }
     }
 
-    val TITLES = arrayOf("Recents", "Favorites", "Nearby", "Music", "Movies")
-    val ICONS = intArrayOf(R.drawable.ic_history, R.drawable.ic_favorite,
-            R.drawable.ic_location, R.drawable.ic_music, R.drawable.ic_movie)
-    val COLORS = intArrayOf(R.color.mdt_cyan_500, R.color.mdt_purple_500, R.color.mdt_red_500,
-            R.color.mdt_indigo_500, R.color.mdt_pink_500)
+    private val COLOR_BGS = intArrayOf(R.drawable.bottom_nav_item_bg_cyan, R.drawable.bottom_nav_item_bg_purple,
+            R.drawable.bottom_nav_item_bg_red, R.drawable.bottom_nav_item_bg_indigo, R.drawable.bottom_nav_item_bg_pink)
+
+    private var mSelectedItem = 0
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_bottom_navigation
@@ -42,33 +40,42 @@ class BottomNavigationFragment : ComponentFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        for (i in 0..arguments.getInt(EXTRA_NUM_TABS) - 1) {
-            bottomNav?.addTab(bottomNav?.newTab(TITLES[i], ContextCompat.getDrawable(activity, ICONS[i])))
+        savedInstanceState?.let {
+            mSelectedItem = it.getInt(BUNDLE_SELECTED_ITEM)
         }
 
         if (arguments.getBoolean(EXTRA_COLORED)) {
-            val newArray = IntArray(arguments.getInt(EXTRA_NUM_TABS))
-            for (i in 0..arguments.getInt(EXTRA_NUM_TABS) - 1) {
-                newArray[i] = COLORS[i]
-            }
-            bottomNav?.setBackgroundColorResources(*newArray)
-            bottomNav?.activeColor = Color.WHITE
-            bottomNav?.setDarkTheme(true)
+            setTabBackgroundColorForPosition(mSelectedItem)
+            bottomNav?.itemIconTintList = ContextCompat.getColorStateList(context, R.color.color_bottom_nav_inverse)
+            bottomNav?.itemTextColor = ContextCompat.getColorStateList(context, R.color.color_bottom_nav_inverse)
         }
 
-        bottomNav?.addOnTabSelectedListener(object : BottomNavigationBar.OnTabSelectedListener {
-            override fun onTabSelected(position: Int) {
-                childFragmentManager.beginTransaction()
-                        .replace(R.id.innerContainer, MockFragment.newInstance(position))
-                        .commit()
+        bottomNav?.setOnNavigationItemSelectedListener { item ->
+            val position = when (item.itemId) {
+                R.id.itemFive -> 4
+                R.id.itemFour -> 3
+                R.id.itemThree -> 2
+                R.id.itemTwo -> 1
+                else -> 0
+            }
+            mSelectedItem = position
+            childFragmentManager.beginTransaction()
+                    .replace(R.id.innerContainer, MockFragment.newInstance(position))
+                    .commit()
+
+            if (arguments.getBoolean(EXTRA_COLORED)) {
+                setTabBackgroundColorForPosition(position)
             }
 
-            override fun onTabUnselected(position: Int) {
-            }
+            true
+        }
 
-            override fun onTabReselected(position: Int) {
-            }
-        })
+        val menuRes = when (arguments.getInt(EXTRA_NUM_TABS)) {
+            5 -> R.menu.menu_bottom_nav_five_items
+            4 -> R.menu.menu_bottom_nav_four_items
+            else -> R.menu.menu_bottom_nav_three_items
+        }
+        bottomNav?.inflateMenu(menuRes)
 
         if (savedInstanceState == null) {
             childFragmentManager.beginTransaction()
@@ -77,8 +84,17 @@ class BottomNavigationFragment : ComponentFragment() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(BUNDLE_SELECTED_ITEM, mSelectedItem)
+    }
+
     override fun onResume() {
         super.onResume()
         (activity as MainActivity).setToolbarTitle("Bottom Navigation Bar")
+    }
+
+    private fun setTabBackgroundColorForPosition(position: Int) {
+        bottomNav?.setBackgroundResource(COLOR_BGS[position])
     }
 }
